311ca1d162015520c2b65b643110329c3049b84a
Daniel Korsah <dkorsah15@aol.co.uk>
2016-10-04 00:05:35 +0100

    Attempt to add Die class for handling all dice operations

---
+++ RollCalculator-Source/WindowsFormsApplication1/Die.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollCalculator
+{
+    class Die
+    {
+        private Random result = new Random();
+        private int x;
+        protected StringBuilder outputString = new StringBuilder();
+
+        public int Roll(int d)
+        {
+            x = result.Next(1, d+1);
+            outputString.Append(x);
+            return x;
+        }
+        public string OutputString()
+        {
+            return outputString.ToString();
+        }
+    } 
+}
