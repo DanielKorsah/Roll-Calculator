36685d4f4fd672f23920aa0c49239060fa2d0fe4
Dan's Laptop <dkorsah15@aol.co.uk>
2018-01-24 16:00:09 +0000

    Basic functionality done.

--- Roll-Calculator2/Roll-Calculator2/MainWindow.xaml.cs
+++ Roll-Calculator2/Roll-Calculator2/MainWindow.xaml.cs
@@ -20,14 +20,33 @@ namespace Roll_Calculator2
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<TextBox> TB;
+
         public MainWindow()
         {
             InitializeComponent();
+            TB = new List<TextBox>(InputsCanvas.Children.OfType<TextBox>().ToList());
         }
 
         private void RollButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Random r = new Random();
+            String allOut = "Output\n";
+            //allOut.Clear();
+            //allOut.Append(Outcomes.Name);
+            foreach (TextBox t in TB)
+            {
+                int sides = Int32.Parse(t.Name.Substring(1));
+                int content = 0;
+                /*int content = */Int32.TryParse(t.Text, out content);
+                for (int i = 0; i < content; i++)
+                {
+                    int output = r.Next(1, sides+1);
+                    allOut += output.ToString() + "\n";
+                    //allOut.AppendLine(output.ToString());
+                }
+            }
+            Outcomes.Text = allOut;
         }
     }
 }
